52bb57a83c748236a6ff61b48052069055e5481a
manitofighh <manitofigh@protonmail.com>
2023-03-31 12:49:40 -0400

    Minor bug fixes and improvements

--- calculator.cpp
+++ calculator.cpp
@@ -35,21 +35,21 @@ int factorial(int num1){ // returns the factorial of any number
 }
 
 void menuOne (){ // the showing menu if the user only wants to work with one number
-   cout << "Choose your required operation: " << endl;
+   cout << "\nChoose your required operation: " << endl;
    cout << "1) ! (factorial)\n2) √ (root)" << endl;
    cin >> detect;
    transform(detect.begin(), detect.end(), detect.begin(), ::tolower);
 }
 
 void menuTwo (){ // the menu if the user wants to work with 2 numbers
-    cout << "Choose your required operation: " << endl;
+    cout << "\nChoose your required operation: " << endl;
     cout << "1) + (sum)\n2) - (substract)\n3) * (multiply)\n4) / (divide)\n5) ^ (power)" << endl;
     cin >> detect;
     transform(detect.begin(), detect.end(), detect.begin(), ::tolower);
 }
 
 void menuOther(){ // the menu when need to take 3 or more numbers into calculation
-    cout << "Choose your required operation: " << endl;
+    cout << "\nChoose your required operation: " << endl;
     cout << "1) + (sum)\n2) - (substract)\n3) * (multiply)\n4) / (divide)"<< endl;
     cin >> detect;
     transform(detect.begin(), detect.end(), detect.begin(), ::tolower);
@@ -65,19 +65,23 @@ int main() {
   string arr6[] = {"1", "factorial", "!", "1)", "one"};
   string arr7[] = {"2", "root", "√", "2)", "two"};
 
-  cout << "\n -------------------------------- \n";
-  cout << " \n|   Welcome to the calculator!  |" << endl;
-  cout << "\n -------------------------------- \n";
-  cout << "How many numbers would you like to enter? ";
+  cout << endl;
+  cout << "************************************" << endl;
+  cout << "***                              ***" << endl;
+  cout << "***  Welcome to the calculator!  *** " << endl;
+  cout << "***                              *** " << endl;
+  cout << "************************************" << endl;
+  cout << "\nHow many numbers would you like to enter? ";
   cin >> userNumAmount;
   vector<double> userVector(userNumAmount); // creates a vector with enough indexes for the amount of numbers the user wants to enter
+  cout << endl;
   for (int i = 1 ; i <= userNumAmount ; i++){
       cout << "Enter number " << i << " : "; // Takes in all the numbers the user wants to enter one after the other
       cin >> userVector.at(i - 1);
   }
   if (userNumAmount == 0) { // since you need at least one number to use the calc, it will ask to enter a num above 0, incase they entered a num < 1
       do {
-          cout << "Only enter an integer above 0: ";
+          cout << "\nOnly enter an integer above 0: ";
           cin >> userNumAmount;
       }
       while (userNumAmount <= 0);
@@ -86,13 +90,13 @@ int main() {
       menuOne();
       if (inArray(detect, arr6, sizeof(arr6) / sizeof(arr6[0]) - 1)) {
           finalNum = factorial(userVector.at(0));
-          cout << "The factorial of " << userVector.at(0) << " is: " << finalNum << endl;
+          cout << "\nThe factorial of " << userVector.at(0) << " is: " << finalNum << endl << endl;
       }
       else if (inArray(detect, arr7, sizeof(arr7) / sizeof(arr7[0]) - 1)) {
           finalNum = sqrt(userVector.at(0));
-          cout << "The square root of " << userVector.at(0) << " is: " << finalNum << endl;
+          cout << "\nThe square root of " << userVector.at(0) << " is: " << finalNum << endl << endl;
       }
-      else cout << "Your input was not evaluated!" << endl;
+      else cout << "\nYour input was not evaluated!" << endl << endl;
   }
   else if (userNumAmount == 2) { // if user wanted to work with 2 numbers
       menuTwo();
@@ -100,34 +104,35 @@ int main() {
           for (int i = 0; i < userNumAmount ; i++) {
               finalNum += userVector.at(i);
           }
-          cout << "The sum of your numbers is: " << finalNum << endl;
+          cout << "\nThe sum of your numbers is: " << finalNum << endl << endl;
       }
       else if (inArray(detect, arr2, sizeof(arr2) / sizeof(arr2[0]) - 1)) {
           finalNum = userVector.at(0);
           for (int i = 1; i < userNumAmount; i++) {
               finalNum -= userVector.at(i);
           }
-          cout << "The subtraction of your numbers is: " << finalNum << endl;
+          cout << "\nThe subtraction of your numbers is: " << finalNum << endl << endl;
       }
       else if (inArray(detect, arr3, sizeof(arr3) / sizeof(arr3[0]) - 1)) {
           finalNum = 1;
           for (int i = 0; i < userNumAmount ; i++) {
               finalNum *= userVector.at(i);
           }
-          cout << "The multiplication of your numbers is: " << finalNum << endl;
+          cout << "\nThe multiplication of your numbers is: " << finalNum << endl << endl;
       }
       else if (inArray(detect, arr4, sizeof(arr4) / sizeof(arr4[0]) - 1)) {
           finalNum = userVector.at(0);
           for (int i = 1; i < userNumAmount; i++) {
               finalNum /= userVector.at(i);
           }
-          cout << "The division of your numbers is: " << finalNum << endl;
+          cout << "\nThe division of your numbers is: " << finalNum << endl << endl;
       }
       else if (inArray(detect, arr5, sizeof(arr5) / sizeof(arr5[0]) - 1)) {
           finalNum = power(userVector.at(0), userVector.at(1));
+          cout << endl;
           cout << userVector.at(0) << " to the power of " << userVector.at(1) << " is: " << finalNum << endl;
       }
-      else cout << "Your input was not evaluated!" << endl;
+      else cout << "\nYour input was not evaluated!" << endl << endl;
   }
   else { // if the numbers to take into calculation were >= 3
       menuOther();
@@ -135,29 +140,29 @@ int main() {
           for (int i = 0; i < userNumAmount ; i++) {
               finalNum += userVector.at(i);
           }
-          cout << "The sum of your numbers is: " << finalNum << endl;
+          cout << "\nThe sum of your numbers is: " << finalNum << endl << endl;
       }
       else if (inArray(detect, arr2, sizeof(arr2) / sizeof(arr2[0]) - 1)) {
           finalNum = userVector.at(0);
           for (int i = 1; i < userNumAmount; i++) {
               finalNum -= userVector.at(i);
           }
-          cout << "The subtraction of your numbers is: " << finalNum << endl;
+          cout << "\nThe subtraction of your numbers is: " << finalNum << endl << endl;
       }
       else if (inArray(detect, arr3, sizeof(arr3) / sizeof(arr3[0]) - 1)) {
           finalNum = 1;
           for (int i = 0; i < userNumAmount ; i++) {
               finalNum *= userVector.at(i);
           }
-          cout << "The multiplication of your numbers is: " << finalNum << endl;
+          cout << "\nThe multiplication of your numbers is: " << finalNum << endl << endl;
       }
       else if (inArray(detect, arr4, sizeof(arr4) / sizeof(arr4[0]) - 1)) {
           finalNum = userVector.at(0);
           for (int i = 1; i < userNumAmount; i++) {
               finalNum /= userVector.at(i);
           }
-          cout << "The division of your numbers is: " << finalNum << endl;
+          cout << "\nThe division of your numbers is: " << finalNum << endl << endl;
       }
-      else cout << "Your input was not evaluated!" << endl; // in case the chosen operation was not detected in the list of options
+      else cout << "\nYour input was not evaluated!" << endl << endl; // in case the chosen operation was not detected in the list of options
   }
 }
